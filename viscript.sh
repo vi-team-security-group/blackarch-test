eb99553d8c21f7036413d2895a9d1c9e75e687dc
mastervi <p3ntt3st@users.noreply.github.com>
2017-03-17 11:04:53 +0200

    Create viscript.sh

---
+++ viscript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+cat <<EOF
+*****************
+ Vi Script
+Update Metasploit
+This script was created by Mastervi
+  
+  mastervi@mail.bg
+*****************
+by Master Vi
+EOF
+echo && echo "Press Enter To Continue ${endc}"
+  read input
+    cd /usr/share/exploitdb
+    rm -rf archive.tar.bz2
+    wget https://github.com/offensive-security/exploit-database/archive/master.zip
+    unzip master.zip
+    rm -rf exploit-database-master.zip
+    echo -e "Done"
+    sleep 3
